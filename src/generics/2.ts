1ffbcef2fbba2ad43cb841600b68dedadf9ffd42
Mariia <masyakuk@gmail.com>
2024-12-20 15:46:02 +0200

    update 2.ts

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -9,9 +9,16 @@ function compare<T extends keyof AllType>(
     top: Pick<AllType, T>,
     bottom: Pick<AllType, T> ): AllType {
  return {
-name: top.name,
-color: top.color,
-position: bottom.position,
-weight: bottom.weight,
+name: top.name as string,
+color: top.color as string,
+position: bottom.position as number,
+weight: bottom.weight as number,
 }
 }
+
+// const result = compare(
+//   { name: "list", color: "black" },
+//   { position: 10, weight: 200 }
+// );
+
+// console.log(result);
